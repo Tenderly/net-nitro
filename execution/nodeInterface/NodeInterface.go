91b06f63b03a3fd8c5cbf11e448d3cbad2a4cade
Nikola Bebić‮ <nikola.bebic@tenderly.co>
2025-05-13 11:53:48 +0200

    fix StateDB cast issues

--- execution/nodeInterface/NodeInterface.go
+++ execution/nodeInterface/NodeInterface.go
@@ -16,7 +16,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/log"
@@ -527,11 +526,7 @@ func (n NodeInterface) GasEstimateL1Component(
 	if err := args.CallDefaults(randomGas, evm.Context.BaseFee, evm.ChainConfig().ChainID); err != nil {
 		return 0, nil, nil, err
 	}
-	sdb, ok := evm.StateDB.(*state.StateDB)
-	if !ok {
-		return 0, nil, nil, errors.New("failed to cast to stateDB")
-	}
-	msg := args.ToMessage(evm.Context.BaseFee, randomGas, n.header, sdb, core.MessageEthcallMode, true, true)
+	msg := args.ToMessage(evm.Context.BaseFee, randomGas, n.header, evm.StateDB, core.MessageEthcallMode, true, true)
 
 	pricing := c.State.L1PricingState()
 	l1BaseFeeEstimate, err := pricing.PricePerUnit()
@@ -587,11 +582,7 @@ func (n NodeInterface) GasEstimateComponents(
 	if err := args.CallDefaults(gasCap, evm.Context.BaseFee, evm.ChainConfig().ChainID); err != nil {
 		return 0, 0, nil, nil, err
 	}
-	sdb, ok := evm.StateDB.(*state.StateDB)
-	if !ok {
-		return 0, 0, nil, nil, errors.New("failed to cast to stateDB")
-	}
-	msg := args.ToMessage(evm.Context.BaseFee, gasCap, n.header, sdb, core.MessageGasEstimationMode, true, true)
+	msg := args.ToMessage(evm.Context.BaseFee, gasCap, n.header, evm.StateDB, core.MessageGasEstimationMode, true, true)
 	brotliCompressionLevel, err := c.State.BrotliCompressionLevel()
 	if err != nil {
 		return 0, 0, nil, nil, fmt.Errorf("failed to get brotli compression level: %w", err)
